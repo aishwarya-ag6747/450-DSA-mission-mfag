5b267dd2ef882822349777c34990f26dc34e820c
Aishwarya Mondal <104521334+aishwarya-ag6747@users.noreply.github.com>
2023-03-01 18:14:06 +0530

    Create battery_health.cpp

---
+++ codechef solution/conditional statement/battery_health/battery_health.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    if(a>=80){
+	        cout<<"Yes"<<endl;
+	    }else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
